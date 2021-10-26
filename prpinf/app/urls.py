8518a58db989aae79f254a2203b96ca27518f1f8
Antonio Roldan Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2021-10-26 18:17:24 +0200

    Tu vieja working

---
+++ prpinf/app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('si/',views.si)
+]
--- prpinf/manage.py
+++ prpinf/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinf.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prpinf.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
