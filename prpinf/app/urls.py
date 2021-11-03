4952b021e52ed7a0e76c37d9f3b2269946296009
Kali <luis.torresmo@alum.uca.es>
2021-11-03 16:59:54 +0000

    ruteado

--- prpinf/app/urls.py
+++ prpinf/app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('si/',views.si)
+    path('home/', views.si)
+    path('foro/', views.si)
+    path('index/', views.si)
 ]
--- prpinf/app/views.py
+++ prpinf/app/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def si(request):
     return render(request,'si.html',{'name':'tu madre L0L'})
+
+def home(request):
+    return render(request, 'home.html')
+
+def foro(request):
+    return render(request, 'foro')
+
+def index(request):
+    return render(request, 'index')
