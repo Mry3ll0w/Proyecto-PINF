f36df2636adc3425c75b55ae647edc0f840eec75
Mry3ll0w <48240649+Mry3ll0w@users.noreply.github.com>
2021-11-05 17:42:09 +0100

    Obtencion de datos del update y tal

--- prpinf/app/models.py
+++ prpinf/app/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 #Las clases deberan tener la primera letra en mayusculas para poder distinguir en caso de usar variables 
 class User(models.Model):
-    first_name = models.CharField(max_length= 30)
+    first_name = models.CharField(max_length= 30,primary_key=True)
     password = models.CharField(max_length= 12)
     done_test = models.BooleanField(default=False)
-    posts = [models.CharField(max_length = 1240 )] # a usar despues
+    
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1440) #280 * 5 (tamaño Twitter * 5)
--- prpinf/app/views.py
+++ prpinf/app/views.py
@@ -3,15 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .forms import UserForm
+from django.db import connections
+
 
 # Create your views here.
-
-
 def post_prueba(request):
     form = UserForm()
     if request.method == 'POST':
-        print(request.POST) 
+        data = request.POST    
     context = {'form':form}
+    
+    print(data['password'])
+    
+
     return render(request, 'prueba_post.html',context)
 
 
