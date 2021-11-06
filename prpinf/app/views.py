b8c485af91f514f6dbad813b8ecab9a8b18f059d
Antonio Roldan Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2021-11-06 12:21:34 +0100

    creadas login y registro
    en login falta rellear cosas

--- prpinf/app/views.py
+++ prpinf/app/views.py
@@ -1,4 +1,5 @@
 
+from django.db.models.query_utils import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
@@ -9,22 +10,53 @@ from django.db import connections
 # Create your views here.
 def post_prueba(request):
     form = UserForm()
-    if request.method == 'POST':
-        print(request.POST)        
+    #if request.method == 'POST':
+        #print(request.POST)        
     context = {'form':form}
-    #print(request.POST.get('mail',''))
-    #insercion en la base de la buena data
-    insertion = User(mail = request.POST.get('mail',''),nickname='' ,password = request.POST.get('password',''),t1_punct=0,t2_punct=0,done_test=False)
-    insertion.save()
-    
+    querys = User.objects.all()
+    token = False
+    for i in querys:
+        if i.mail == request.POST.get('mail',''):
+            token = True
 
-
-    
+    if token == False: #creamos el user y tal
+        insertion = User(mail = request.POST.get('mail',''),nickname='' ,password = request.POST.get('password',''),t1_punct=0,t2_punct=0,done_test=False)
+        insertion.save()
 
     return render(request, 'prueba_post.html',context)
 
+def login(request):
+    form = UserForm()
+    #if request.method == 'POST':
+        #print(request.POST)        
+    context = {'form':form}
+    querys = User.objects.all()
+    token = False
+    for i in querys:
+        if i.mail == request.POST.get('mail',''):
+            token = True
+
+    if token == True: #creamos el user y tal
+        print('user encontrado')#rellenar con return de datos a html
+    return render(request, 'login.html',context)
 
 
+def registro(request):#codigo sin su html y tal
+    form = UserForm()
+    #if request.method == 'POST':
+        #print(request.POST)        
+    context = {'form':form}
+    querys = User.objects.all()
+    token = False
+    for i in querys:
+        if i.mail == request.POST.get('mail',''):
+            token = True
+
+    if token == False: #creamos el user y tal
+        insertion = User(mail = request.POST.get('mail',''),nickname='' ,password = request.POST.get('password',''),t1_punct=0,t2_punct=0,done_test=False)
+        insertion.save()
+
+    return render(request, 'registro.html',context)
 
 
 def home(request):
@@ -39,8 +71,6 @@ def index(request):
 def perfil(request):
     return render(request, 'perfil.html')
 
-def registro(request):
-    return render(request, 'registro.html')
 
 def test(request):
     return render(request, 'test.html')
