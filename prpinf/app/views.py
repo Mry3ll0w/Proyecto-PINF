e29b6f64ae0f5a60aef2ab0361c47f36e51cc55f
PC Casa <luis.torresmo@alum.uca.es>
2021-11-16 16:53:30 +0100

    LISTO

--- prpinf/app/views.py
+++ prpinf/app/views.py
@@ -42,13 +42,8 @@ from django.contrib.auth.decorators import login_required
 
 def post_prueba(request):
 
-    if request.user.is_authenticated:
 
-        return redirect('http://127.0.0.1:8000/app/prueba_login/')
-
-    else:
-
-        if request.method == 'POST':
+    if request.method == 'POST':
 
             #Siempre que trabajemos con un formulario, tenemos que comprobar que sea valido, esto se hace con la instruccion form.is_vail()
 
@@ -56,64 +51,53 @@ def post_prueba(request):
 
                 #Ahora vamos a obtener la data limpia del form, [usamos form.cleaned_data.get]esto se puede hacer de muchas formas pero esta me parece la mas clara
 
-            mail = request.POST['mail']
-            nickname = request.POST['nickname']
-            password = request.POST['password']
+        mail = request.POST['mail']
+        nickname = request.POST['nickname']
+        password = request.POST['password']
 
-            new_user = User(nickname = nickname, mail = mail, password = password, t1_punct = 0, t2_punct = 0, done_test= False)
-            new_user.save()
+        new_user = User(nickname = nickname, mail = mail, password = password, t1_punct = 0, t2_punct = 0, done_test= False)
+        new_user.save()
 
-            messages.success(request, 'Se ha creado la cuenta')
-            return redirect('http://127.0.0.1:8000/app/prueba_login/')
+        messages.success(request, 'Se ha creado la cuenta')
+        return redirect('http://127.0.0.1:8000/app/prueba_login/')
 
-        return render(request, 'prueba_post.html') 
+    return render(request, 'prueba_post.html') 
 
 #------------------------------------------------------LOGIN---------------------------------------------------------------------------------------
 
 def login_prueba(request):
 
-    if request.user.is_authenticated:
-
-        return redirect('http://127.0.0.1:8000/app/home/')
-
-    form = UserFormLogin()
-
     if request.method == 'POST':
-
-        form = LoginForm(request.POST)
-        context = {'form':form}
 
         #Hay que verificar que el formato es valido, sino no se puede usar el cleaned.data
 
-        if form.is_valid():
 
-            #Tambien se pueden obtener los datos con el request.POST, ambas formas son igualmente validas
+         #Tambien se pueden obtener los datos con el request.POST, ambas formas son igualmente validas
 
-            post_nickname = request.POST['nickname']
-            post_password = request.POST['password']
+        post_nickname = request.POST['nickname']
+        post_password = request.POST['password']
 
-            querys = User.objects.all()
-            isRegistered = False
+        querys = User.objects.all()
+        isRegistered = False
 
             #Vale a ver, django puede hacer el login por la cara, pero eso da una especie de permisos que no nos vienen al cuento
             #   asi que para nuestro login simplente verificaremos si el usuario esta en la base de datos y obviamente que sea la
             #   misma password.
 
-            for i in querys:
+        for i in querys:
 
-                if i.nickname == post_nickname and i.password == post_password:
+            if i.nickname == post_nickname and i.password == post_password:
 
-                        isRegistered = True
+                    isRegistered = True
 
-            if isRegistered == True:
+        if isRegistered == True:
 
-                return redirect('http://127.0.0.1:8000/app/home/')
+            return redirect('http://127.0.0.1:8000/app/home/')
 
-            else:
-                return redirect('http://127.0.0.1:8000/prueba_login/')
+        else:
+            return redirect('http://127.0.0.1:8000/prueba_login/')
 
-    context = {'form':form}
-    return render(request, 'prueba_login.html', context) 
+    return render(request, 'prueba_login.html') 
  
 # --------------------------------------------------- PRUEBA_POLL -------------------------------------------------------------------------- #
 def prueba_poll(request): #OJO este es un endpoint de prueba para hacer otro (NO ES EL DEFINITIVO)
