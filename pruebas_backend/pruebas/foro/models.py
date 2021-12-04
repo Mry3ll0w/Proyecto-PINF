b9ebaa72c6608a8db5b9d6130979937a2486b95f
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-12-04 16:57:26 +0100

    initial shit pruebas foro

--- pruebas_backend/pruebas/foro/models.py
+++ pruebas_backend/pruebas/foro/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    author  = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1440) #280 * 5 (tamaño Twitter * 5)
--- pruebas_backend/pruebas/foro/views.py
+++ pruebas_backend/pruebas/foro/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def foro(request):
+    if request.method == 'GET':
+        return render(request,'foro.html',)
