38d6acec8e78efaf187691ad7f2de676731cbda9
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-12-06 16:09:02 +0100

    Cositas en pruebas para el foro

--- pruebas_backend/pruebas/foro/urls.py
+++ pruebas_backend/pruebas/foro/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     
-    path('home/',views.foro)
+    path('home/',views.foro),
+    path('create_post',views.create_post)
+    
 ]
--- pruebas_backend/pruebas/foro/views.py
+++ pruebas_backend/pruebas/foro/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User
 
 def foro(request):
     if request.method == 'GET':
-        return render(request,'foro.html',)
+        posts = Post.objects.all()
+        for i in posts:
+            print(i.post_id)
+            print(i.author)
+            print(i.content)
+        
+        return render(request,'foro.html')
+
+def create_post(request):
+    if request.method == 'POST':
+        users = User.objects.all() 
+        input = request.POST #guarda el contenido
+        token = False
+
+        #Primero buscaremos si ese usuario existe en la base de datos
+        for i in users:
+            if i.username == input['author']:
+                print('El usuario existe')
+                token = True
+
+        #Comprobamos si se ha encontrado
+        if token==False: #No se encuentra se redirige al home
+            return redirect('http://127.0.0.1:8000/foro/create_post')
+        else:#Se encuentra entonces lo creo
+            
+            p = Post(author=User.objects.get(username=input['author']), content=input['content'])
+            p.save()#Se guardan los datos y se redirige al home donde se ven los post
+            return redirect('http://127.0.0.1:8000/foro/home')
+            
+    return render(request, 'create_post.html')
