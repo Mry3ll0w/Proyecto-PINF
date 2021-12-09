15300edbac3b9954871eb5a368548634885fc80a
Antonio Roldan Andrade <antonioroldanandrade@gmail.com>
2021-12-09 10:31:13 +0100

    Comentarios y cambios en pruebas
    He puesto comentarios y tal para que el Don Jefe de Backend no me fune

--- pruebas_backend/pruebas/foro/views.py
+++ pruebas_backend/pruebas/foro/views.py
@@ -3,35 +3,49 @@ from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.models import User
 
+#Se encarga de mostrar el foro
 def foro(request):
     if request.method == 'GET':
-        posts = Post.objects.all()
-        for i in posts:
-            print(i.post_id)
-            print(i.author)
-            print(i.content)
-        
-        return render(request,'foro.html',{'db':posts})#le pasamos la base de datos en el context para que la muestre
 
+        posts = Post.objects.all()#Metemos todas las querys de los post en la DB en post
+
+        return render(request,'foro.html',{'db':posts})#le pasamos la base de datos en el context para que la muestre
+    
+    else:
+        #En caso de tener otro tipo de acceso que no sea el correspondiente se manda un 500 (forbidden operation)
+        return HttpResponse(status=403)
+
+
+#Crea un post para meterlo en la DB
 def create_post(request):
+
     if request.method == 'POST':
+        #Variables
         users = User.objects.all() 
         input = request.POST #guarda el contenido
         token = False
 
         #Primero buscaremos si ese usuario existe en la base de datos
         for i in users:
+            
             if i.username == input['author']:
+            
                 print('El usuario existe')
+            
                 token = True
+
 
         #Comprobamos si se ha encontrado
         if token==False: #No se encuentra se redirige al home
+            
             return redirect('http://127.0.0.1:8000/foro/create_post')
-        else:#Se encuentra entonces lo creo
+
+        else:#Se encuentra==> lo creo
             
             p = Post(author=User.objects.get(username=input['author']), content=input['content'])
+        
             p.save()#Se guardan los datos y se redirige al home donde se ven los post
+            
             return redirect('http://127.0.0.1:8000/foro/home')
             
     return render(request, 'create_post.html')
